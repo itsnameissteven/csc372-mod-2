0ffd6cab4feeded2482d8cb5578f5d972f7d349f
Steven Mancine <steven.mancine@charter.com>
2024-04-18 18:56:55 -0600

    Finish main funcs

--- src/BankAccountInterface.java
+++ src/BankAccountInterface.java
@@ -4,55 +4,105 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class BankAccountInterface extends JFrame implements ActionListener {
-  private JLabel initialBalanceLabel, balanceLabel, depositLabel, withdrawalLabel;
-  private JTextField initialBalanceField, balanceField, depositField, withdrawalField;
+  private JLabel initialBalanceLabel, balanceLabel, actionLabel;
+  private JTextField initialBalanceField, balanceField, actionField;
   private JButton submitButton, depositButton, withdrawButton;
   private JPanel welcomeScreen, menuScreen;
-  private CardLayout view;
   private double balance = 0.0;
 
 
   BankAccountInterface() {
     setTitle("Bank Account");
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setSize(300, 200);
-
+    setLocationRelativeTo(null);
+    setSize(600, 400);
 
     // Welcome screen
-    welcomeScreen = new JPanel();
+    welcomeScreen = new JPanel(new GridBagLayout());
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.insets = new Insets(5, 5, 5, 5);
+
     initialBalanceLabel = new JLabel("What is your initial balance? ");
     initialBalanceField = new JTextField(10);
     submitButton = new JButton("Submit");
     submitButton.addActionListener(this);
-    welcomeScreen.add(initialBalanceLabel);
-    welcomeScreen.add(initialBalanceField);
-    welcomeScreen.add(submitButton);
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    welcomeScreen.add(initialBalanceLabel, gbc);
+    gbc.gridx = 1;
+    welcomeScreen.add(initialBalanceField, gbc);
+    gbc.gridx = 0;
+    gbc.gridy = 1;
+    gbc.gridwidth = 2;
+
+    welcomeScreen.add(submitButton, gbc);
 
     // Menu screen
-    menuScreen = new JPanel();
+    // Display current balance
+    menuScreen = new JPanel(new GridBagLayout());
     balanceLabel = new JLabel("Your current balance: ");
-    balanceField = new JTextField(String.valueOf(balance));
-    menuScreen.add(balanceLabel);
-    menuScreen.add(balanceField);
+    balanceField = new JTextField(String.valueOf(balance), 10);
+    balanceField.setEditable(false);
+    gbc.gridwidth = 1;
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    menuScreen.add(balanceLabel, gbc);
+    gbc.gridx = 1;
+    menuScreen.add(balanceField, gbc);
 
+    // One input to deposit or withdraw
+    actionLabel = new JLabel("Amount to withdraw/deposit");
+    actionField = new JTextField("0.00", 10);
+    gbc.gridx = 0;
+    gbc.gridy = 1;
+    menuScreen.add(actionLabel, gbc);
+    gbc.gridx = 1;
+    menuScreen.add(actionField, gbc);
 
-    // balanceLabel = new JLabel("Your current balance: ");
-    // depositLabel = new JLabel("How much do you want to deposit?");
-    // withdrawalLabel = new JLabel("How much do you want to withdraw");
-    add(welcomeScreen);
-    // add(menuScreen);
+    // Use buttons to decide which action to take.
+    depositButton = new JButton("Deposit");
+    depositButton.addActionListener(this);
+    withdrawButton = new JButton("Withdraw");
+    withdrawButton.addActionListener(this);
+    gbc.gridx = 0;
+    gbc.gridy = 2;
+    menuScreen.add(depositButton,gbc);
+    gbc.gridx = 1;
+    menuScreen.add(withdrawButton, gbc);
 
+    // Initialize the program with the welcome screen
+    setContentPane(welcomeScreen);
   }
 
   @Override
   public void actionPerformed(ActionEvent event) {
+    // Each action should validate the input
     if(event.getSource() == submitButton) {
       try {
         balance += Double.parseDouble(initialBalanceField.getText());
         balanceField.setText(String.valueOf(balance));
+        // Switch screen
         setContentPane(menuScreen);
         repaint();
         revalidate();
+      } catch (Exception e) {
+        JOptionPane.showMessageDialog(this, "Please enter a valid number");
+      } 
+    } else if (event.getSource() == depositButton) {
+      try {
+        // Deposit balance
+        balance += Double.parseDouble(actionField.getText());
+        balanceField.setText(String.valueOf(balance));
+        actionField.setText("0.00");
+      } catch (Exception e) {
+        JOptionPane.showMessageDialog(this, "Please enter a valid number");
+      } 
+    } else if (event.getSource() == withdrawButton) {
+      try {
+        // Withdraw balance
+        balance -= Double.parseDouble(actionField.getText());
+        balanceField.setText(String.valueOf(balance));
+        actionField.setText("0.00");
       } catch (Exception e) {
         JOptionPane.showMessageDialog(this, "Please enter a valid number");
       } 
@@ -61,7 +111,6 @@ public class BankAccountInterface extends JFrame implements ActionListener {
 
   public static void main(String[] args) {
     BankAccountInterface frame = new BankAccountInterface();
-    frame.pack();
     frame.setVisible(true);
   }
 }
